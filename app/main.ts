f9ff0d7f523243909e6cdadec05e5712f4800154
Angular0527 <Angular0527@outlook.com>
2016-11-24 16:04:48 +0300

    First version

---
+++ app/main.ts
@@ -0,0 +1,10 @@
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS, XHRBackend } from '@angular/http';
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS,
+    { provide: XHRBackend, useClass: InMemoryBackendService },
+]);
